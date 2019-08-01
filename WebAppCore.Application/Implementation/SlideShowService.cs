a596f685c0d59a627783479247c05799e27dcae0
Nghia Nguyen <37038823+nghia89@users.noreply.github.com>
2019-08-01 09:46:39 +0700

    add service slide

---
+++ WebAppCore.Application/Implementation/SlideShowService.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebAppCore.Application.Interfaces;
+using WebAppCore.Application.ViewModels.Common;
+using WebAppCore.Data.Entities;
+using WebAppCore.Infrastructure.Interfaces;
+using WebAppCore.Utilities.Dtos;
+
+namespace WebAppCore.Application.Implementation
+{
+    public class SlideShowService : ISlideShowService
+    {
+        private readonly IRepository<Slide, int> _SlideRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public SlideShowService(IRepository<Slide, int> SlideRepository, IUnitOfWork unitOfWork)
+        {
+            _SlideRepository = SlideRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public void Create(SlideShowViewModel slideVM)
+        {
+            var slide = Mapper.Map<SlideShowViewModel, Slide>(slideVM);
+            _SlideRepository.Add(slide);
+        }
+
+        public void DeleteDetail(int id)
+        {
+            _SlideRepository.Remove(id);
+        }
+
+        public PagedResult<SlideShowViewModel> GetAllPaging(int pageIndex, int pageSize)
+        {
+            var query = _SlideRepository.FindAll();
+            int totalRow = query.Count();
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            var data = query.ProjectTo<SlideShowViewModel>().ToList();
+
+            var paginationSet = new PagedResult<SlideShowViewModel>()
+            {
+                Results = data,
+                CurrentPage = pageIndex,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
+            return paginationSet;
+
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(SlideShowViewModel slideVM)
+        {
+            var slide = Mapper.Map<SlideShowViewModel, Slide>(slideVM);
+            _SlideRepository.Update(slide);
+        }
+    }
+}
---
+++ WebAppCore/Areas/Admin/Controllers/SlideShowController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebAppCore.Application.Interfaces;
+using WebAppCore.Application.ViewModels.Common;
+
+namespace WebAppCore.Areas.Admin.Controllers
+{
+    public class SlideShowController : Controller
+    {
+        private readonly ISlideShowService _slideShowService;
+
+        public SlideShowController(ISlideShowService slideShowService)
+        {
+            _slideShowService = slideShowService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public IActionResult GetById(int id)
+        //{
+        //    var model = _slideShowService.GetDetail(id);
+
+        //    return new OkObjectResult(model);
+        //}
+
+        public IActionResult SaveEntity(SlideShowViewModel slideShow)
+        {
+            if (!ModelState.IsValid)
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+            if (slideShow.Id == 0)
+            {
+                _slideShowService.Create(slideShow);
+            }
+            else
+            {
+                _slideShowService.Update(slideShow);
+            }
+            _slideShowService.Save();
+            return new OkObjectResult(slideShow);
+        }
+    }
+}
