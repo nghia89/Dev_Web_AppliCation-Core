2d84075b079b35b70c16f7c448329e726682fd5f
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2020-03-27 08:42:59 +0700

    update multiple image

---
+++ WebAppCore/Models/UtilModel/FileImageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore.Models.UtilModel
+{
+    public class FileImageModel
+    {
+        public bool Status { get; set; }
+        public string FileName { get; set; }
+    }
+}
