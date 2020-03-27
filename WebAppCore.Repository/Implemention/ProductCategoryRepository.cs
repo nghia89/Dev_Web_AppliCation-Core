f528d4d914375b901a2090c9e4ce687c0c73c9b7
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2020-03-27 23:44:12 +0700

    get flase sale by categoty children by parent

--- WebAppCore.Repository/Implemention/ProductCategoryRepository.cs
+++ WebAppCore.Repository/Implemention/ProductCategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAppCore.Data.EF;
@@ -9,56 +10,73 @@ using WebAppCore.Repository.Interfaces;
 
 namespace WebAppCore.Repository.Implemention
 {
-	public class ProductCategoryRepository:IProductCategoryRepository
-	{
-		private AppDbContext _appDbContext;
-		private IRepository<ProductCategory,int> _productCategory;
-		private IRepository<Product,int> _productRepository;
-		public ProductCategoryRepository(AppDbContext appDbContext,IRepository<ProductCategory,int> productCategory,
-			IRepository<Product,int> productRepository)
-		{
-			this._appDbContext = appDbContext;
-			this._productCategory = productCategory;
-			this._productRepository = productRepository;
-		}
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private AppDbContext _appDbContext;
+        private IRepository<ProductCategory, int> _productCategory;
+        private IRepository<Product, int> _productRepository;
+        public ProductCategoryRepository(AppDbContext appDbContext, IRepository<ProductCategory, int> productCategory,
+            IRepository<Product, int> productRepository)
+        {
+            this._appDbContext = appDbContext;
+            this._productCategory = productCategory;
+            this._productRepository = productRepository;
+        }
 
-		public async Task<List<ProductCategory>> GetAll()
-		{
-			var data = await _productCategory.FindAllAsync(x => x.Status == Status.Active);
-			return data.ToList();
-		}
+        public async Task<List<ProductCategory>> GetAll()
+        {
+            var data = await _productCategory.FindAllAsync(x => x.Status == Status.Active);
+            return data.ToList();
+        }
 
-		public async Task<ProductCategory> GetById(long id)
-		{
-			var data = await _productCategory.GetAByIdIncludeAsyn(x => x.Id == id);
-			return data;
-		}
+        public async Task<ProductCategory> GetById(long id)
+        {
+            var data = await _productCategory.GetAByIdIncludeAsyn(x => x.Id == id);
+            return data;
+        }
 
-		public async Task<List<ProductCategory>> GetHomeCategories(int top)
-		{
-			var query = await _productCategory
-				 .FindAllAsync(x => x.HomeFlag == true);
+        public async Task<List<ProductCategory>> GetHomeCategories(int top)
+        {
+            var query = await _productCategory
+                 .FindAllAsync(x => x.HomeFlag == true && x.ParentId == null);
 
-			var categories = query.OrderByDescending(x => x.HomeOrder)
-				   .Take(top).ToList();
+            var categories = query.OrderByDescending(x => x.HomeOrder)
+                   .Take(top).ToList();
 
-			foreach(var category in categories)
-			{
-				var product = await _productRepository.FindAllAsync(x => x.HomeFlag == true && x.CategoryId == category.Id);
-				category.Products = product.OrderByDescending(x => x.DateCreated)
-					.Take(top).ToList();
-			}
-			return categories;
-		}
+            foreach (var category in categories)
+            {
+                var idChildren = await this.GetChildrenByParent(category.Id);
+                var product = await _productRepository.FindAllAsync(x => x.HomeFlag == true && idChildren.Contains(x.CategoryId));
+                category.Products = product.OrderByDescending(x => x.DateCreated)
+                    .Take(top).ToList();
+            }
+            return categories;
+        }
 
-		public Task<List<ProductCategory>> GetProductNew(int top)
-		{
-			throw new System.NotImplementedException();
-		}
 
-		public Task<(List<ProductCategory> data, long totalCount)> Paging(int page,int page_size)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
+        public async Task<List<int?>> GetChildrenByParent(int id)
+        {
+            var listId = new List<int?>();
+            var data = await _appDbContext.Set<ProductCategory>().Where(a => a.ParentId == id).ToListAsync();
+
+
+            while (data.Count() > 0 && data != null)
+            {
+                var ids = data.Select(a => (int?)a.Id).ToList();
+                listId.AddRange(ids);
+                data = await _appDbContext.Set<ProductCategory>().Where(a => ids.Contains(a.ParentId)).ToListAsync();
+            }
+            return listId;
+        }
+
+        public Task<List<ProductCategory>> GetProductNew(int top)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<(List<ProductCategory> data, long totalCount)> Paging(int page, int page_size)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
