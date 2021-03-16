904b93a57c88cf4ab99191c41cc2ee34a5722db0
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2021-03-16 12:39:12 +0700

    up pack

--- Dapper/Implementation/ReportService.cs
+++ Dapper/Implementation/ReportService.cs
@@ -1,10 +1,11 @@
-﻿using Dapper;
+﻿
+
+using Dapper;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Text;
 using System.Threading.Tasks;
 using WebAppCore.Application.Dapper.Interfaces;
 using WebAppCore.Application.Dapper.ViewModels;
